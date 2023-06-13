7a371052dfd0d299fe88fc75e993003d213592b1
Pedro.J (Pitter) <pedrojc@outlook.es>
2023-06-13 20:19:23 +0200

    Inital commit

---
+++ app.js
@@ -0,0 +1,17 @@
+'use stritc';
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+
+btn.addEventListener('click',function(){
+    //get random num between 0-3 ->colors[0]
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor=colors[randomNumber];
+    color.textContent=colors[randomNumber];
+})
+
+function getRandomNumber(){
+    return parseInt(Math.random()*colors.length);
+}
